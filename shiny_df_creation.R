378fa70af12458f3290932eb2feda0700618f8e3
mattwittbrodt <mattwittbrodt@gmail.com>
2019-09-12 13:57:12 -0400

    starting outline of shiny DF creation

--- shiny_df_creation.R
+++ shiny_df_creation.R
@@ -1,7 +1,24 @@
-
 # DFS 2019 PreProcessing for Shiny App
 library(tidyverse)
 
-# Vegas Lines
+
+# Vegas Lines -------------------------------------------------------------
+
 source("~/ff_shiny_app/ff_app/vegas_lines.R")
 vegas <- vegas_lines()
+
+
+# Positions ---------------------------------------------------------------
+source("~/ff_shiny_app/ff_app/position_information.R")
+
+# QB
+qb <- position_stats("QB",2)
+
+# RB
+rb <- position_stats("RB",2)
+
+# WR
+wr <- position_stats("RB",2)
+
+# TE
+te <- position_stats("TE",2)
