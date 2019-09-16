983cb163e20e67b8ac21ffb5cb4f7e6c4af43968
mattwittbrodt <mattwittbrodt@gmail.com>
2019-09-16 17:10:39 -0400

    adding the DVOA offense and defense

--- shiny_df_creation.R
+++ shiny_df_creation.R
@@ -15,8 +15,11 @@ vegas[["team"]] <- sapply(vegas[["team"]], function(x) find_names(x, "vegas"))
 # DVOA Data ---------------------------------------------------------------
 source("~/ff_shiny_app/ff_app/dvoa.R")
 dvoa <- dvoa() 
-dvoa[["team"]] <- sapply(dvoa[["team"]], function(x) find_names(x, "fff_abbreviation"))
+dvoa_defense <- dvoa$defense
+dvoa_offense <- dvoa$offense
 
+dvoa_defense[["team"]] <- sapply(dvoa_defense[["team"]], function(x) find_names(x, "fff_abbreviation"))
+dvoa_offense[["team"]] <- sapply(dvoa_offense[["team"]], function(x) find_names(x, "fff_abbreviation"))
 
 # Positions ---------------------------------------------------------------
 source("~/ff_shiny_app/ff_app/position_information.R")
@@ -40,6 +43,8 @@ all_positions <- merge(qb,rb, all = TRUE) %>%
                  merge(wr, all = TRUE) %>%
                  merge(te, all = TRUE) %>%
                  merge(vegas, by.x = "tm", by.y = "team") %>%
-                 merge(dvoa, by.x = "opp", by.y = "team")
+                 merge(dvoa_defense, by.x = "opp", by.y = "team") %>%
+                 merge(dvoa_offense, by.x = "tm", by.y = "team")
 
-writexl::write_xlsx(all_positions, "~/ff_shiny_app/all_data_wk_2.xlsx")
+# Adding Dvoc
+#writexl::write_xlsx(all_positions, "~/ff_shiny_app/all_data_wk_2.xlsx")
