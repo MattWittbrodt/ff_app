7c0a99c50395ddee62e81f06564967b6beebed04
mattwittbrodt <mattwittbrodt@gmail.com>
2019-09-24 09:07:18 -0400

    working version for vegas output with new site

--- vegas_lines.R
+++ vegas_lines.R
@@ -1,6 +1,7 @@
 # Getting Vegas Lines
+# date is string formatted "mm/dd"
 
-vegas_lines <- function() {
+vegas_lines <- function(date) {
   
   library(rvest)
   library(tidyverse)
@@ -14,7 +15,7 @@ vegas_lines <- function() {
   
   # New data cleaning
   df <- d %>% 
-         filter(str_detect(X1,"/") == T & str_detect(X1, "09/22") == T) %>%
+         filter(str_detect(X1,"/") == T & str_detect(X1, date) == T) %>%
          select(X1,X3) %>%
          .[-1,]
   
@@ -25,7 +26,7 @@ vegas_lines <- function() {
                 total = str_replace(total, "u-10", " "),
                 total = str_remove(total, "-\\d{2}$"),
                 game = str_remove_all(game, "\\."),
-                game = str_remove(game, "09/22\\s+"),
+                game = str_remove(game, paste(date,"\\s+",sep = "")),
                 game = str_remove(game, "PM\\s+"),
                 teams = str_extract_all(game, "(?<=\\d{3}\\s)(\\w{5}\\s\\w{3}|\\w{4,}|\\w{2,}\\s\\w{2,})"),
                 home = map_chr(teams, last),
@@ -44,7 +45,8 @@ vegas_lines <- function() {
            favorite = ifelse(team == favorite, 1, 0),
            line = ifelse(favorite == 1, as.numeric(line), as.numeric(line)*-1),
            total = as.numeric(str_remove_all(total, "\\s")),
-           implied_total = (total/2)+((line*-1)/2))
+           implied_total = (total/2)+((line*-1)/2),
+           team = str_replace_all(team, "\\s", "_"))
   
   
   return(df2)
