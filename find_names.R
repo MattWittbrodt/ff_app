eb4b8f3ef82f25410ced2b692824e5199787098c
mattwittbrodt <mattwittbrodt@gmail.com>
2019-10-08 22:15:05 -0400

    adding a local call for excel file

--- find_names.R
+++ find_names.R
@@ -1,6 +1,9 @@
 #finding team names
 find_names <- function(x,col) {
   
+  # Read in CSV
+  tm_names <- readxl::read_xlsx("~/ff_shiny_app/ff_app/data/team_names.xlsx")
+  
   # Checking if the opponent is listed as '@' - or away
   if(str_detect(x,"^@") == T) {
     
