0be88733337f0981ceaacc8e3419429aa62cb61b
mattwittbrodt <mattwittbrodt@gmail.com>
2019-11-14 10:31:35 -0500

    starting the merging docs

--- data_merging.R
+++ data_merging.R
@@ -1,8 +1,15 @@
 #### test
+library(tidyverse)
 
 f <- as.list(list.files("~/ff_shiny_app/ff_app/data/",
                 pattern = "all_data",
                 full.names = T)
 )
 
-f2 <- lapply(f, readxl::read_xlsx)
+f2 <- lapply(f, function(x) {
+  d <- readxl::read_xlsx(x)
+  return(t(as.data.frame(names(d))))
+  }) %>%
+  bind_cols(fill = NA)
+
+
