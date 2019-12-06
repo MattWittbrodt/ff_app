e20bfeeaa29905006d5b9871a980588cd0659756
mattwittbrodt <mattwittbrodt@gmail.com>
2019-12-06 10:00:38 -0500

    fixing issue where csv is not available for download- converting from saved html now

--- wr_vs_cb.R
+++ wr_vs_cb.R
@@ -6,21 +6,25 @@ wr_vs_cv <- function(file) {
   library(tidyverse)
   
   # Read in file
-  d <- read.csv(file) %>%
-       select(-Day, -Tm, -Pos, -CB, -M.U, -YPT, -Opp) %>%
-       mutate(Tar.. = as.numeric(str_remove(Tar.., "%")),
-              C.. = as.numeric(str_remove(C.., "%")),
-              matchup = factor(ifelse(X..... == " ", "plus",
-                              ifelse(X..... == "N", "neutral", "minus")), 
+  d <- read_html(file) %>% 
+       html_table() %>%
+       .[[1]] %>%
+       select(-Day, -Tm, -Pos, -CB, -'M/U', -YPT, -Opp) %>%
+       mutate('Tar %' = as.numeric(str_remove(`Tar %`, "%")),
+              'C %' = as.numeric(str_remove(`C %`, "%")),
+              matchup = factor(ifelse("(+/-)" == " ", "plus",
+                              ifelse("(+/-)" == "N", "neutral", "minus")), 
                               ordered = T, levels = c("minus", "neutral", "plus")),
               WR = str_remove_all(as.character(WR), "[:punct:]")) %>%
-       select(-X.....)
+       select(-"(+/-)")
   
   names(d) <- names(d) %>%
               str_to_lower() %>%
               str_remove_all("[:punct:]")
   
   names(d) <- paste("vs_cb", names(d), sep = "_")
+  
+  write.csv(d, str_replace(file, ".html", ".csv"))
   
   return(d)
 }
