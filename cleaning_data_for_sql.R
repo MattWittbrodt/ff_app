b6d20989fabb0588f48485ebb8af9512cf81d840
mattwittbrodt <mattwittbrodt@gmail.com>
2020-03-07 16:59:56 -0500

    getting ready for name separation

--- cleaning_data_for_sql.R
+++ cleaning_data_for_sql.R
@@ -1,7 +1,13 @@
 # Combined table into players for database
 
+
+
+# Reading in players- getting the unique ones -----------------------------
 d <- readxl::read_xlsx("C:/Users/mattw/Documents/ff_shiny_app/tmp_update_for_SQL.xlsx") %>%
-     .[,c(2:4)]
+     .[,c(2:4)] %>%
+     unique() %>%
+     mutate(proj_player = str_split(proj_player, " "),
+            first_name = NA,
+            last_name = NA)
 
 
-## Getting unique players
