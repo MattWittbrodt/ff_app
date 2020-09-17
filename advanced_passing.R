f82d85eb5ddbae73b2f6d2b4f7331c1a30f36feb
mattwittbrodt <mattwittbrodt@gmail.com>
2020-09-17 18:00:57 -0500

    rushing works

--- advanced_passing.R
+++ advanced_passing.R
@@ -6,7 +6,7 @@ advanced_passing_stats <- function() {
            read_html() %>%
            html_table(fill = T)
   
-  # 
+  # returned tables are in a list, so returning those
   dfs <- lapply(link, function(x) {
     
     # Making better column names which are in the first row
@@ -41,8 +41,6 @@ advanced_passing_stats <- function() {
                  -adv_passing_yds) %>%
           filter(adv_player != "Player")
     
-    print("The last table processing has been completed")
-      
     return(x2)
     
   })
@@ -68,6 +66,52 @@ advanced_rushing_stats <- function() {
   
   link2 <- read_html(link_comments) %>% html_table(fill = T)
   link_data <- link2[[1]]
+  
+  # Fixing column names, which are a combination of column names and first row
+  descriptions <- link_data[1,]
+  link_data <- link_data[-1,] # removing first row
+  
+  names <- colnames(link_data)
+  
+  # Creating a combination column name
+  for(ii in 1:length(names)) {
+    
+    names[ii] <- paste("adv",names[ii],descriptions[ii], sep = "_") %>%
+      str_to_lower() %>%
+      str_replace("__","_") %>%
+      str_replace("/", "_per_") %>%
+      str_replace("%","_per")
+  }
+  
+  # Add back into the DF
+  colnames(link_data) <- names
+  
+  # Selecting onl the relevant columns
+  link_data2 <- link_data %>%
+                select(-adv_rk,
+                       -adv_tm,
+                       -adv_age,
+                       -adv_pos,
+                       -adv_games_g,
+                       -adv_games_gs,
+                       -adv_rushing_att,
+                       -adv_rushing_yds) %>%
+                filter(adv_player != "Player")
+  
+  return(link_data2)
+  
+}
+
+# Merging into one DF
+all_df <- left_join(dfs[[1]], dfs[[2]], by = "adv_player") %>%
+  left_join(dfs[[3]], by = "adv_player") %>%
+  left_join(dfs[[4]], by = "adv_player")
+
+# Fixing a few issues with columns
+all_df[,-1] <- apply(all_df[,-1], c(1,2), function(x) {str_remove(x, "%") %>% as.numeric()})
+
+return(all_df)
+  
   
 }
   
