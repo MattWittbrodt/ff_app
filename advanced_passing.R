4af8d813e6789a5896d7b3d8c7d079978a3e3722
mattwittbrodt <mattwittbrodt@gmail.com>
2020-09-17 07:29:46 -0500

    cleaning the data frames

--- advanced_passing.R
+++ advanced_passing.R
@@ -10,23 +10,33 @@ advanced_passing_stats <- function() {
     
     print(x)
     
-    # Make first row into column names
-    colnames(x) <- x[1,]
-    x <- x[-1,]
+    # Making better column names which are in the first row
+    descriptions <- x[1,]
+    x <- x[-1,] # removing first row
     
+    names <- colnames(x)
+    
+    # Creating a combination 
+    for(ii in 1:length(names)) {
+      
+      names[ii] <- paste("adv",names[ii],descriptions[ii], sep = "_") %>%
+                   str_to_lower() %>%
+                   str_replace("__","_")
+    }
+    print(names)
     # Alter / standardize the column names - adding adv_passing as prefix
-    names <- colnames(x) %>% str_to_lower()
+    #names <- colnames(x) %>% str_to_lower()
     
-    names <- paste("adv_passing",names, sep = "_")
+    #names <- paste("adv",names, sep = "_")
     colnames(x) <- names
     
     # Fixing a few issues with columns
     x2 <- x %>%
-          select(-adv_passing_rk,
-                 -adv_passing_tm,
-                 -adv_passing_age,
-                 -adv_passing_pos,
-                 -adv_passing_g,
+          select(-adv_rk,
+                 -adv_tm,
+                 -adv_age,
+                 -adv_pos,
+                 -adv_games_g,
                  -adv_passing_gs,
                  -adv_passing_cmp,
                  -adv_passing_att,
