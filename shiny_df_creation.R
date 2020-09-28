8c1037b46a1eb46aa0854107470a4013234fb6d7
mattwittbrodt <mattwittbrodt@gmail.com>
2020-09-27 22:01:11 -0500

    adding the advanced FO stats for each player

--- shiny_df_creation.R
+++ shiny_df_creation.R
@@ -1,7 +1,7 @@
 # DFS 2019 PreProcessing for Shiny App
 
 shiny_df <- function(wk_num,date) {
-  
+
 library(tidyverse)
 
 # Reading in team name chart ----------------------------------------------
@@ -16,7 +16,7 @@ print('Vegas Lines Successful')
 
 # DVOA Data ---------------------------------------------------------------
 source("~/ff_shiny_app/ff_app/dvoa.R", local = T)
-dvoa <- dvoa(playoffs = FALSE) 
+dvoa <- dvoa(playoffs = FALSE)
 dvoa_defense <- dvoa$defense
 dvoa_offense <- dvoa$offense
 
@@ -27,7 +27,7 @@ print('DVOA Successful')
 # Last Week Data ----------------------------------------------------------
 
 wk_data <- lapply(list("QB", "RB", "WR", "TE"), function(position) {
-          
+
           # If week is 1, use last year's data
           wk_data <-  paste("https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=2020&year_max=2020&season_start=1&season_end=-1&pos%5B%5D=",
                               position,
@@ -40,7 +40,7 @@ wk_data <- lapply(list("QB", "RB", "WR", "TE"), function(position) {
                               sep = "") %>%
                       read_html() %>%
                       html_table(fill = T) %>%
-                      .[[1]] 
+                      .[[1]]
 })
 
 print("Last Week Data Successful")
@@ -50,7 +50,7 @@ rz_types <- list("passing","rushing","receiving")
 
 rz_df <- lapply(rz_types,
                 function(x) {
-                  
+
                   # Reading in Data
                   d <- paste("http://www.pro-football-reference.com/years/2020/redzone-",
                              x,
@@ -59,7 +59,7 @@ rz_df <- lapply(rz_types,
                     read_html() %>%
                     html_table(fill = T) %>%
                     .[[1]]
-                  
+
                   # Ammending names
                   rz_names <- paste(colnames(d), d[1,]) %>%
                     str_to_lower() %>%
@@ -72,16 +72,16 @@ rz_df <- lapply(rz_types,
                     str_replace_all("%(?=\\w)","per_") %>%
                     str_replace_all("\\s+","_") %>%
                     str_replace_all("cmp","comp")
-                  
-                  
+
+
                   # Adding into new columns
                   colnames(d) <- paste(x,rz_names,sep = "_")
-                  
+
                   # Final few cleaning steps
                   d <- d %>%
                     subset(.[[paste(x,"player",sep = "_")]] != "Player") %>%
                     select(-contains("link"))
-                  
+
                 })
 # Fixing Names
 rz_df <- lapply(rz_df, function(x) {name_fixes(x, 1, 2, 0)})
@@ -91,7 +91,7 @@ print("Red Zone Offensive Data Successful")
 # Year to Date Data -------------------------------------------------------
 
 ytd_df <- lapply(list("passing","rushing","receiving"), function(x) {
-  
+
   # Reading in Data
   d <- paste("http://www.pro-football-reference.com/years/2020/",
              x,
@@ -101,24 +101,24 @@ ytd_df <- lapply(list("passing","rushing","receiving"), function(x) {
     html_table(fill = T) %>%
     .[[1]] %>%
     .[,-1]
-  
+
   if(x == "passing" | x == "receiving") {
     d <- d  %>%
       subset(Pos != "Pos") %>%
       select(-Age, -GS, -Lng) %>%
       mutate(Pos = str_to_upper(Pos))
-    
+
   } else {
-    
+
     # Rush is divided into two rows
     colnames(d) <- d[1,]
     d <- d[-1,] %>%
       subset(Pos != "Pos" & as.numeric(Att) > 4) %>%
       select(-Age, -GS, -Lng) %>%
       mutate(Pos = str_to_upper(Pos))
-    
+
   }
-  
+
   # Ammending names
   d_names <- colnames(d) %>%
     str_to_lower() %>%
@@ -135,7 +135,7 @@ ytd_df <- lapply(list("passing","rushing","receiving"), function(x) {
     str_replace("y/g", "yds_per_gm") %>%
     str_replace("y/r", "yds_per_rec") %>%
     str_replace("y/target", "yds_per_target")
-  
+
   # Adding prefix to column names
   if(x == "rushing") {
     colnames(d) <- paste("ytd_rush", d_names, sep = "_")
@@ -146,20 +146,20 @@ ytd_df <- lapply(list("passing","rushing","receiving"), function(x) {
       colnames(d) <- paste("ytd_pass", d_names, sep = "_")
     }
   }
-  
+
   # Converting the catch % into numeric
   if(x == "receiving") {
     d[["ytd_rec_ctch_per"]] <- as.numeric(sapply(d[["ytd_rec_ctch_per"]],
                                                  function(x) str_remove(x, '%')))
   }
-  
+
   # Making Columns numeric
-  d[,c(4:length(d))] <- apply(d[,c(4:length(d))], 
-                              2, 
+  d[,c(4:length(d))] <- apply(d[,c(4:length(d))],
+                              2,
                               function(x) as.numeric(as.character(x)))
-  
+
   # Making a few columns per game
-  
+
   if(x == "passing") {
     d <- mutate(d,
                 ytd_pass_comp = round(ytd_pass_comp / ytd_pass_g,2),
@@ -182,7 +182,7 @@ ytd_df <- lapply(list("passing","rushing","receiving"), function(x) {
                   ytd_rush_yds = round(ytd_rush_yds / ytd_rush_g,2),
                   ytd_rush_td = round(ytd_rush_td / ytd_rush_g,2))
     }}
-  
+
 })
 
 ytd_df <- lapply(ytd_df, function(x) {name_fixes(x, 1, 2, 3)})
@@ -257,8 +257,8 @@ leverage <- read.csv(paste("~/ff_shiny_app/ff_app/data/4for4-gpp-leverage-scores
                    Tm = as.character(Tm),
                    Player = str_remove_all(Player, "[:punct:]"))
 
-leverage_names <- colnames(leverage) %>% 
-                  str_to_lower() %>% 
+leverage_names <- colnames(leverage) %>%
+                  str_to_lower() %>%
                   str_remove("[.]+$") %>%
                   str_replace("[.]","_")
 
@@ -286,19 +286,45 @@ adv_pass <- advanced_passing_stats()
 adv_rush <- advanced_rushing_stats()
 adv_rec <- advanced_receiving_stats()
 
-# Adding in individual Football Outsider Stats ----
-source("~/ff_shiny_app/ff_app/football_outsiders_advanced_position.R")
-
 # Joining into one large dataframe
-all_data <- all_positions %>% 
-            left_join(adv_pass, by = c("proj_player" = "adv_player")) %>% 
-            left_join(adv_rush, by = c("proj_player" = "adv_player")) %>% 
+all_data <- all_positions %>%
+            left_join(adv_pass, by = c("proj_player" = "adv_player")) %>%
+            left_join(adv_rush, by = c("proj_player" = "adv_player")) %>%
             left_join(adv_rec, by = c("proj_player" = "adv_receiving_player"))
 
 print("Advanced Stats Successful")
 
+# Adding in individual information from Football Outsiders ----
+source("~/ff_shiny_app/ff_app/football_outsiders_advanced_position.R")
+
+# Running specific Data
+qb_df <- fo_qb()
+rb_df <- fo_rb()
+wr_df <- fo_pass_catchers("wr")
+te_df <- fo_pass_catchers("te")
+
+# Merging into 1 DF
+fo_all_positions <- full_join(qb_df, rb_df, by = c("pass_player" = "rush_player")) %>%
+                    full_join(wr_df, by = c("pass_player" = "rec_player")) %>%
+                    full_join(te_df, by = c("pass_player" = "rec_player")) %>%
+                    mutate(pass_player = ifelse(is.na(str_extract(pass_player, "^[:upper:]\\.[:upper:]\\.")) == T,
+                                        pass_player,
+                                        str_remove(pass_player, "\\.")),
+                    pass_player = str_replace(pass_player, "[:punct:]", " "),
+                    pass_player = str_remove_all(pass_player, "-"))
+fo_all_positions[["pass_team"]] <- sapply(fo_all_positions[["pass_team"]], function(x) find_names(x, "fff_abbreviation"))
+
+# Merging with rest of data
+all_data2 <- all_data %>%
+             mutate(proj_player_new = str_remove_all(proj_player, "(?<=[:upper:])[:alpha:]{1,}(?=[:space:])"))
+
+
+# Combining
+all_data_fo_pos <- left_join(all_data, fo_all_positions, by = c("proj_player_new" = "pass_player", "proj_tm" = "pass_team")) %>%
+                   select(-proj_player_new)
+
 # Returning full DF ----
-return(all_data)
+return(all_data_fo_pos)
 }
 
 # Getting full dataframe --------------------------------------------------
