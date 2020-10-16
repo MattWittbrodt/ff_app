04a0616b54bdade32e1754b9fe9bef4443f7e8d2
mattwittbrodt <mattwittbrodt@gmail.com>
2020-10-16 07:58:06 -0500

    moving to csv files again, and also fixing issues where + isn't rendering - will have to check if continual issue

--- wr_vs_cb.R
+++ wr_vs_cb.R
@@ -4,27 +4,27 @@
 wr_vs_cv <- function(file) {
 
   library(tidyverse)
-  
+
   # Read in file
-  d <- read_html(file) %>% 
-       html_table() %>%
-       .[[1]] %>%
+  d <- read_csv(file) %>%
+       # html_table() %>%
+       # .[[1]] %>%
        select(-Day, -Tm, -Pos, -CB, -'M/U', -YPT, -Opp) %>%
        mutate('Tar %' = as.numeric(str_remove(`Tar %`, "%")),
               'C %' = as.numeric(str_remove(`C %`, "%")),
-              matchup = factor(ifelse(`(+/-)` == "+", "plus",
-                              ifelse(`(+/-)` == "N", "neutral", "minus")), 
+              matchup = factor(ifelse(is.na(`(+/-)`) == T, "plus",
+                              ifelse(`(+/-)` == "N", "neutral", "minus")),
                               ordered = T, levels = c("minus", "neutral", "plus")),
               WR = str_remove_all(as.character(WR), "[:punct:]")) %>%
        select(-"(+/-)")
-  
+
   names(d) <- names(d) %>%
               str_to_lower() %>%
               str_remove_all("[:punct:]")
-  
+
   names(d) <- paste("vs_cb", names(d), sep = "_")
-  
+
   write.csv(d, str_replace(file, ".html", ".csv"))
-  
+
   return(d)
 }
