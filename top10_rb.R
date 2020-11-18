9230df887d506488be1b797b79865c82201183fe
Matt Wittbrodt <mattwittbrodt@gmail.com>
2020-11-18 11:02:44 -0600

    week 11 analysis

--- top10_rb.R
+++ top10_rb.R
@@ -1,27 +1,22 @@
 ## Top 10
-
 library(tidyverse)
 
 wk_num <- 11
 
-pos <- "QB"
+pos <- "RB"
 
 d <- readxl::read_xlsx("C:/Users/mattw/Documents/ff_shiny_app/ff_app/2020_data/merged_data/2020_weeks_2to10_combined.xlsx") %>%
-     filter(proj_week <= wk_num - 1 & proj_week > 3 & proj_pos == pos)
-
-cur_wk <- readxl::read_xlsx("C:/Users/mattw/Documents/ff_shiny_app/ff_app/data/all_data_wk_10_2020.xlsx") %>%
-          filter(proj_pos == pos)
-
+  filter(proj_week <= wk_num - 1 & proj_week > 3 & proj_pos == pos)
 
 # need previous week fantasy data, so reading in this week's data to look at
 tw <- readxl::read_xlsx(paste0("C:/Users/mattw/Documents/ff_shiny_app/ff_app/data/all_data_wk_",wk_num,"_2020.xlsx")) %>%
-      filter(proj_pos == pos) %>%
-      select(proj_player, prev_wk_fantasy_fdpt)
+  filter(proj_pos == pos) %>%
+  select(proj_player, prev_wk_fantasy_fdpt)
 
 weeks <- unique(d$proj_week) 
 
 compiled_data <- lapply(as.list(weeks), function(x) {
-
+  
   # Getting last week and current week data
   wk <- filter(d, proj_week == x) %>% select(-prev_wk_fantasy_fdpt)
   
@@ -47,67 +42,57 @@ compiled_data <- lapply(as.list(weeks), function(x) {
 # Putting into a DF and doing some QB-specific processing
 processed_data <- do.call(rbind, compiled_data)
 
-qb_col <- c("proj_pos","proj_player","prev_wk_fantasy_fdpt","proj_week",
+rb_col <- c("proj_pos","proj_player","prev_wk_fantasy_fdpt","proj_week",
             "pts_vs_g", "projected_own","proj_afpa_rk","line","total",
-            "implied_total","pts_vs_passing_att", "pts_vs_passing_yds", "pts_vs_passing_td",
-            "pts_vs_fantasy_per_game_fdpt","def_red_zone_td","def_red_zone_pct",
-            "def_dvoa", "def_pass_dvoa", "def_dline_pass_rank",
-            "def_dline_pass_adjusted_sack_rate","def_pass_qb_rating_allowed",
-            "def_pass_adj_net_yds_per_att", "off_oline_pass_adjusted_sack_rate",
-            "ytd_pass_yds_per_gm","ytd_pass_td","adv_passing_iay","pass_dyar",
-            "adv_passing_ontgt_per","adv_passing_bad_per",
-            "adv_passing_prss_per","pass_eyds","pass_yards","rush_eyds","rush_dyar","rush_yards",
-            "passing_twenty_att","passing_twenty_td","top_ten")
+            "implied_total", "ytd_rush_att", "pts_vs_fantasy_per_game_fdpt",
+            "pts_vs_rec_tgt","pts_vs_rec_yds","pts_vs_rec_td",
+            "pts_vs_rush_att","pts_vs_rush_yds","pts_vs_rush_td","high_value_touches","rush_eyds","rush_yards",
+            "rec_eyds","rec_yards","rec_dyar","rush_dyar",
+            "def_rush_yds_per_att","def_red_zone_td","def_red_zone_pct","ytd_rush_g",
+            "def_dvoa","def_rush_dvoa","off_rush_dvoa","def_dline_power_success",
+            "def_dline_adj_line_yards","def_dline_stuffed","def_dline_2nd_level_yards",
+            "def_dline_open_field_yards","off_oline_adj_line_yards","off_oline_power_success",
+            "off_oline_stuffed","off_oline_open_field_yards","off_oline_2nd_level_yards",
+            "ytd_rec_target", "top_ten")
 
-processed_data <- select(processed_data, qb_col) %>%
-                  mutate(adv_passing_iay = round(adv_passing_iay / as.numeric(pts_vs_g), 2),
-                         pass_dyar = round(pass_dyar/as.numeric(pts_vs_g),2),
-                         rush_yards = round(rush_yards / as.numeric(pts_vs_g),2),
-                         pass_yds_diff = round((pass_eyds - pass_yards)/as.numeric(pts_vs_g),2),
-                         rush_yds_diff = round((rush_eyds - rush_yards)/as.numeric(pts_vs_g),2),
-                         DVOA_Diff = def_pass_dvoa - def_dvoa,
-                         pts_vs_passing_att = round(as.numeric(pts_vs_passing_att) / as.numeric(pts_vs_g),2),
-                         pts_vs_passing_yds = round(as.numeric(pts_vs_passing_yds) / as.numeric(pts_vs_g),2),
-                         pts_vs_passing_td = round(as.numeric(pts_vs_passing_td) / as.numeric(pts_vs_g),2),
-                         pts_vs_fantasy_per_game_fdpt = as.numeric(pts_vs_fantasy_per_game_fdpt)) %>%
-                  select(-pts_vs_g)
+processed_data <- select(processed_data, rb_col) %>%
+  mutate(pts_vs_g = as.numeric(pts_vs_g),
+         pts_vs_rec_tgt = as.numeric(pts_vs_rec_tgt)/pts_vs_g,
+         pts_vs_rec_yds = as.numeric(pts_vs_rec_yds)/pts_vs_g,
+         pts_vs_rec_td = as.numeric(pts_vs_rec_td)/pts_vs_g,
+         pts_vs_rush_att = as.numeric(pts_vs_rush_att)/pts_vs_g,
+         pts_vs_rush_yds = as.numeric(pts_vs_rush_yds)/pts_vs_g,
+         pts_vs_rush_td = as.numeric(pts_vs_rush_td)/pts_vs_g,
+         pts_vs_total_touch = pts_vs_rush_att + pts_vs_rec_tgt,
+         DVOA_Advantage = def_rush_dvoa + off_rush_dvoa,
+         DVOA_Difference = def_rush_dvoa - def_dvoa,
+         net_adj_line_yd_diff = off_oline_adj_line_yards - (def_dline_adj_line_yards*-1),
+         power_success_diff = off_oline_power_success - def_dline_power_success,
+         second_level_yds_diff = as.numeric(off_oline_2nd_level_yards) - as.numeric(def_dline_2nd_level_yards),
+         open_fieldyards_diff = as.numeric(off_oline_open_field_yards) - as.numeric(def_dline_open_field_yards),
+         ytd_rec_target = ifelse(is.na(ytd_rec_target) == T, 0, ytd_rec_target),
+         total_touches = ytd_rush_att + ytd_rec_target,
+         rush_eyard_diff = round((rush_eyds - rush_yards)/ytd_rush_g,2),
+         rec_eyard_diff = round((rec_eyds - rec_yards)/ytd_rush_g,2),
+         rec_dyar = round(rec_dyar/ytd_rush_g,2),
+         rush_dyar = round(rush_dyar/ytd_rush_g,2)) %>%
+  select(-pts_vs_g, ytd_rush_g)
 
-cur_wk_processed <- select(cur_wk, qb_col[-length(qb_col)]) %>%
-                  mutate(adv_passing_iay = round(adv_passing_iay / as.numeric(pts_vs_g), 2),
-                         pass_dyar = round(pass_dyar/as.numeric(pts_vs_g),2),
-                         rush_yards = round(rush_yards / as.numeric(pts_vs_g),2),
-                         pass_yds_diff = round((pass_eyds - pass_yards)/as.numeric(pts_vs_g),2),
-                         rush_yds_diff = round((rush_eyds - rush_yards)/as.numeric(pts_vs_g),2),
-                         DVOA_Diff = def_pass_dvoa - def_dvoa,
-                         pts_vs_passing_att = round(as.numeric(pts_vs_passing_att) / as.numeric(pts_vs_g),2),
-                         pts_vs_passing_yds = round(as.numeric(pts_vs_passing_yds) / as.numeric(pts_vs_g),2),
-                         pts_vs_passing_td = round(as.numeric(pts_vs_passing_td) / as.numeric(pts_vs_g),2),
-                         pts_vs_fantasy_per_game_fdpt = as.numeric(pts_vs_fantasy_per_game_fdpt)) %>%
-                  select(-pts_vs_g)
 
 # Cleaning up data (for entire dataset)
 processed_data[,-c(2:3)] <- apply(processed_data[,-c(2:3)], 2,
-                            function(x) {c <- str_remove_all(as.character(x), "%") %>% as.numeric()})
-
-cur_wk_processed[,-c(2:3)] <- apply(cur_wk_processed[,-c(2:3)], 2,
                                   function(x) {c <- str_remove_all(as.character(x), "%") %>% as.numeric()})
-
 
 
 # For now, doing a median replace for NA's
 # TODO - smarter NA replacement
 #for(ii in c(5:36,38:40)) {processed_data[,ii][is.na(processed_data[,ii])] <- median(unlist(processed_data[,ii]), na.rm = T)}
 for(ii in c(4:ncol(processed_data))) {processed_data[,ii][is.na(processed_data[,ii])] <- median(unlist(processed_data[,ii]), na.rm = T)}
-for(ii in c(4:ncol(cur_wk_processed))) {cur_wk_processed[,ii][is.na(cur_wk_processed[,ii])] <- median(unlist(cur_wk_processed[,ii]), na.rm = T)}
 
 # Normalizing
-processed_data[,c(6:36,38:40)] <- apply(processed_data[,c(6:36,38:40)], 2, function(x) {m <- mean(x); sd = sd(x); x-m/sd})
+#processed_data[,c(6:36,38:40)] <- apply(processed_data[,c(6:36,38:40)], 2, function(x) {m <- mean(x); sd = sd(x); x-m/sd})
+processed_data[,c(5:42,44:53)] <- apply(processed_data[,c(5:42,44:53)], 2, function(x) {m <- mean(x); sd = sd(x); x-m/sd})
 processed_data$top_ten <- as.factor(processed_data$top_ten)
-
-
-cur_wk_processed[,c(6:ncol(cur_wk_processed))] <- apply(cur_wk_processed[,c(6:ncol(cur_wk_processed))], 2, function(x) {m <- mean(x); sd = sd(x); x-m/sd})
-
-
 
 # Running Random Forest and PCA -----
 library(randomForest)
@@ -117,10 +102,10 @@ test <- filter(processed_data, proj_week == wk_num - 1)
 
 # Creating a formula for the RF
 formula <- "top_ten ~ "
-for(n in colnames(processed_data[,c(6:36,38:40)])) {if(n != 'DVOA_Diff') {formula <- paste0(formula,n," + ")} else {formula <- paste0(formula, n)}}
+for(n in colnames(processed_data[,c(6:42,44:53)])) {if(n != 'rec_eyard_diff') {formula <- paste0(formula,n," + ")} else {formula <- paste0(formula, n)}}
 formula<- as.formula(formula)
 
-# Getting PCA on data
+# Running the Random Forest
 classifier <- randomForest(formula,
                            data = train,
                            ntree = 10000,
@@ -132,7 +117,7 @@ classifier
 
 ## Getting metrics on training data
 training_pred <- predict(classifier, type = "prob")
-train$pred <- training_pred[,2]
+train$pred <- training_pred[,1]
 
 ## Running a sensitivity analysis on decision marker
 # Sensitivity Analysis for threshold -----
@@ -156,7 +141,7 @@ for(ii in c(0.30,0.40,0.45,0.50,0.55,0.60,0.65)) {
   
   # Getting MSE
   wd$sq_error <- (as.numeric(as.character(wd$top_ten)) - wd$pred)^2
-
+  
   # Print result
   cat(paste0("Threshold of: ",ii,
              " = Accuracy: ", round(accuracy*100,2),
@@ -170,14 +155,9 @@ for(ii in c(0.30,0.40,0.45,0.50,0.55,0.60,0.65)) {
 
 ## Test dataset testing ----
 test_pred <- predict(classifier, newdata = test, type = "prob")
-test$pred <- test_pred[,2]
+test$pred <- test_pred[,1]
 
-cur_wk_pred <- predict(classifier, newdata = cur_wk_processed, type = "prob")
-cur_wk_processed$pred <- cur_wk_pred[,2]
-cur_wk_processed$top_ten <- ifelse(cur_wk_processed$pred > 0.5, 1, 0)
-predictions <- select(cur_wk_processed, proj_player, top_ten)
-
-test$pred_binomial <- ifelse(test$pred > 0.5, 1, 0)
+test$pred_binomial <- ifelse(test$pred > 0.55, 1, 0)
 
 test$tp = ifelse(test$pred_binomial == 1 & test$top_ten == 1,1,0)
 test$fp = ifelse(test$pred_binomial == 1 & test$top_ten == 0,1,0)
