96127aa8410263dc74ce1c3a3d996176f767dbfe
Matt Wittbrodt <mattwittbrodt@gmail.com>
2021-07-05 11:21:27 -0500

    from data we currently have - for SQL

--- ff_python/2020_schedule_cleaning.py
+++ ff_python/2020_schedule_cleaning.py
@@ -27,4 +27,42 @@ d['reformat_week'] = d.apply(lambda row: f'0{row.week}' if row.week < 10 else f'
 # Greating game id similar to pro football ref: 202009130jax {year}{date}{win}...{year}{week}{home}{away}
 d['game_id'] = d.apply(lambda row: f'{row.season}{row.reformat_week}{row.home_team.lower()}{row.away_team.lower()}', axis = 1)
 
+# %% Clean up - set index as game_id and remove reformatted week
+d.set_index('game_id', inplace=True)
+d.drop('reformat_week', axis = 1, inplace=True)
+
+# %% Now, add vegas stuff
+v = pd.read_excel('C:/Users/mattw/Documents/ff_shiny_app/ff_app/2020_data/merged_data/2020_weeks_2to17_combined.xlsx')
+v = v[['proj_week', 'proj_tm','proj_opp','line','total']]
+v.drop_duplicates(inplace = True)
+
+# %% Merging the two
+# In this case, if the line is negative, the home team is favored.
+c = pd.merge(d,v, how = "left", 
+            left_on = ['home_team','week'], 
+            right_on = ['proj_tm','proj_week']).set_index(d.index)
+c.drop(['proj_week','proj_tm','proj_opp'], axis = 1, inplace = True)
+
+# %% Calculating the home and away implied total
+# Implied total = (Total / 2) +/- (spread/2)
+def implied_calc(total,line,away):
+    # for away teams
+    if away == 1 and line < 0:
+        corrected_line = line
+    elif away == 1 and line > 0:
+        corrected_line = line
+    # for home team
+    elif away == 0 and line < 0:
+        corrected_line = line * -1
+    else:
+        corrected_line = line*-1
+           
+    return (total/2) + (corrected_line/2) 
+
+c['home_implied'] = c.apply(lambda row: implied_calc(row.total, row.line, 0), axis = 1)
+c['away_implied'] = c.apply(lambda row: implied_calc(row.total, row.line, 1), axis = 1)
+
+# %% Write out
+c.to_csv('C:/Users/mattw/Desktop/2020_games.csv', index=True, header=True)
+
 # %%
